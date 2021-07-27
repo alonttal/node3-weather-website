fb3ec535e9de2ecc355f5766a2654cedec8cd82e
Alon <alontalmor@gmail.com>
2021-07-27 21:13:23 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=f18b32cc887d5abfc8adc0523a77b236&query=' + latitude + ',' + longitude + '&units=f'
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!')
+    } else if (body.error) {
+      callback('Unable to find location')
+    } else {
+      console.log(body.current)
+      callback(undefined,
+        body.current.weather_descriptions[0] + '. ' +
+        'It is currently ' + body.current.temperature + ' degrees out. ' +
+        'It feels like ' + body.current.feelslike + ' degrees out.'
+      )
+    }
+  })
+}
+
+module.exports = forecast
